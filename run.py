c0985baa7cbb0dd6e7f76f09ff4a502ef6cb7d34
DWAA1660 <dwatnip123@gmail.com>
2025-12-11 08:46:35 -0700

    edit blog

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from patient_eggs import create_app, db
-from patient_eggs.models import User, Product, InventoryAdult, InventoryEggWeekly, GalleryImage
+from patient_eggs.models import User, Product, InventoryAdult, InventoryEggWeekly, GalleryImage, BlogPost
 
 app = create_app()
 
